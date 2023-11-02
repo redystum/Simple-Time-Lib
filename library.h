1d9fb87062235b0c0de754acdca74b634c103b17
redystum <rffarffarffa@gmail.com>
2023-11-02 23:13:18 +0000

    unix to datetime, datetime_to_unix, docs

--- library.h
+++ library.h
@@ -28,14 +28,14 @@ typedef struct {
 
 /// Returns the current unix time
 /// @return time in seconds since 1970
-static int unix_time() {
+int unix_time() {
     time_t t = time(NULL);
     return (int) t;
 }
 
 /// Returns the current date and time
 /// @return datetime struct
-static datetime current_datetime() {
+datetime current_datetime() {
     time_t t = time(NULL);
     struct tm tm = *localtime(&t);
     datetime dt = {tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec};
@@ -53,7 +53,7 @@ static datetime current_datetime() {
 /// %s: second \n
 /// (default: %Y-%M-%D %h:%m:%s)
 /// @return string
-static char *datetime_to_string(const datetime dt, const char *format) {
+char *datetime_to_string(const datetime dt, const char *format) {
     if (format == NULL || format[0] == '\0') {
         char *str = malloc(20 * sizeof(char));
         sprintf(str, "%d-%d-%d %d:%d:%d", dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second);
@@ -102,10 +102,29 @@ static char *datetime_to_string(const datetime dt, const char *format) {
     return str;
 }
 
-char *unix_to_string(const char *format) {
-    int unix = unix_time();
-    datetime dt = current_datetime();
-    dt.year = unix;
+/// Returns the unix time as a formatted string
+/// @param unix time in seconds since 1970
+/// @return datetime struct
+datetime unix_to_datetime(const int unix) {
+    time_t t = unix;
+    struct tm tm = *localtime(&t);
+    datetime dt = {tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec};
+    return dt;
+}
+
+/// Returns the unix time as a formatted string
+/// @param unix time in seconds since 1970
+/// @param format string \n
+/// %Y: year \n
+/// %M: month \n
+/// %D: day \n
+/// %h: hour \n
+/// %m: minute \n
+/// %s: second \n
+/// (default: %Y-%M-%D %h:%m:%s)
+/// @return string
+char *unix_to_string(const int unix, const char *format) {
+    datetime dt = unix_to_datetime(unix);
     return datetime_to_string(dt, format);
 }
 
@@ -122,6 +141,15 @@ char *unix_to_string(const char *format) {
 char *current_datetime_string(const char *format) {
     datetime dt = current_datetime();
     return datetime_to_string(dt, format);
+}
+
+/// Converts a datetime struct to unix time
+/// @param dt datetime struct
+/// @return unix time in seconds since 1970
+int datetime_to_unix(const datetime dt) {
+    struct tm tm = {dt.second, dt.minute, dt.hour, dt.day, dt.month - 1, dt.year - 1900};
+    time_t t = mktime(&tm);
+    return (int) t;
 }
 
 #endif //SIMPLE_TIME_LIBRARY_H
--- test.c
+++ test.c
@@ -4,10 +4,16 @@
 #include <time.h>
 
 int main() {
-    printf("unix_time: %d\nform time.h : %lld", unix_time(), time(NULL));
+    int unix = unix_time();
+    printf("unix_time: %d\nform time.h : %lld", unix,  time(NULL));
     datetime dt = current_datetime();
     printf("current datetime: %d-%d-%d %d:%d:%d\n", dt.year, dt.month,
            dt.day, dt.hour, dt.minute, dt.second);
     printf("\n\n%s\n", current_datetime_string("%D-%M-%Y random text %h:%m: aaa%s %Y---%M-%D te%Mxt"));
+    datetime dt2 = unix_to_datetime(unix);
+    printf("unix to datetime: %d-%d-%d %d:%d:%d\n", dt2.year, dt2.month,
+           dt2.day, dt2.hour, dt2.minute, dt2.second);
+    printf("\nformatted unix to str %s", unix_to_string(unix, "%D-%M-%Y random text %h:%m: aaa%s %Y---%M-%D te%Mxt"));
+    printf("\n datetime to unix: %d", datetime_to_unix(dt));
     return 0;
 }
